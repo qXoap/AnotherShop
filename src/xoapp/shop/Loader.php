2df7394805467a125def96fe17a122680ee4e64b
qXoap <92841319+qXoap@users.noreply.github.com>
2025-04-05 21:05:58 -0500

    first commit

--- src/xoapp/shop/Loader.php
+++ src/xoapp/shop/Loader.php
@@ -2,7 +2,23 @@
 
 namespace xoapp\shop;
 
-class Loader
-{
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
 
+class Loader extends PluginBase
+{
+    use SingletonTrait {
+        setInstance as private;
+        reset as private;
+    }
+
+    protected function onEnable(): void
+    {
+        self::setInstance($this);
+    }
+
+    protected function onDisable(): void
+    {
+
+    }
 }
